492e6a479e6869f4c3df838403e949110170c5e7
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-04 17:02:22 -0600

    change empty_board array

--- lib/board.rb
+++ lib/board.rb
@@ -1,25 +1,39 @@
 class Board
 
   def empty_board
-    a = [["A1", " "], ["A2", " "], ["A3", " "], ["A4", " "]]
-    b = [["B1", " "], ["B2", " "], ["B3", " "], ["B4", " "]]
-    c = [["C1", " "], ["C2", " "], ["C3", " "], ["C4", " "]]
-    d = [["D1", " "], ["D2", " "], ["D3", " "], ["D4", " "]]
-    render_board(a, b, c, d)
+    [
+      ["A1", " "], ["A2", " "], ["A3", " "], ["A4", " "],
+      ["B1", " "], ["B2", " "], ["B3", " "], ["B4", " "],
+      ["C1", " "], ["C2", " "], ["C3", " "], ["C4", " "],
+      ["D1", " "], ["D2", " "], ["D3", " "], ["D4", " "]
+    ]
+    render_board
   end
 
-  def render_board(a, b, c, d)
+  def render_board
     puts ". 1 2 3 4"
-    puts "A #{print_row(a)}"
-    puts "B #{print_row(b)}"
-    puts "C #{print_row(c)}"
-    puts "D #{print_row(d)}"
+    puts "A"
+    puts "B"
+    puts "C"
+    puts "D"
   end
 
   def print_row(row)
     spot = ""
     row.each {|space| spot = spot + space[1]}
     spot
+  end
+
+  def shot(coordinate)
+    if coordinate != " "
+      "this location has already been fired upon, please choose another coordinate"
+    else
+
+    end
+  end
+
+  def ship
+
   end
 
 end
