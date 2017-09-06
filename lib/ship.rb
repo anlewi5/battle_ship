373b3464031dcdee3a3d14cc0b4ce35686de247f
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-05 20:22:39 -0600

    add ship tests and make ship methods pass tests

--- lib/ship.rb
+++ lib/ship.rb
@@ -8,32 +8,32 @@ class Ship
     @end_coord = coordinate_array[1]
   end
 
-  def ship_orientation(start_coord, end_coord, ship_length)
-    if start_coord.last == end_coord.last
-      horizontal_ship(start_coord, end_coord, ship_length)
-    elsif start_coord.first == end_coord.first
-      vertical_ship(start_coord, end_coord, ship_length)
+  def ship_orientation(ship_length)
+    if start_coord[1] == end_coord[1]
+      horizontal_ship(ship_length)
+    elsif start_coord[0] == end_coord[0]
+      vertical_ship(ship_length)
     else
       "ship is invalid: cannot place diagonal"
     end
   end
 
-    def horizontal_ship(start_coord, end_coord, ship_length)
-      check_horizontal_ship_length(start_coord, end_coord, ship_length)
+    def horizontal_ship(ship_length)
+      check_horizontal_ship_length(ship_length)
     end
 
-      def check_horizontal_ship_length(start_coord, end_coord, ship_length)
-        if abs(start_coord.first.ord - end_coord.first.ord) != ship_length - 1
+      def check_horizontal_ship_length(ship_length)
+        if (start_coord[0].ord - end_coord[0].ord).abs != ship_length - 1
           "ship is invalid: incorrect ship length"
         end
       end
 
-    def vertical_ship(start_coord, end_coord, ship_length)
-      check_vertical_ship_length(start_coord, end_coord, ship_length)
+    def vertical_ship(ship_length)
+      check_vertical_ship_length(ship_length)
     end
 
-      def check_vertical_ship_length(start_coord, end_coord, ship_length)
-        if abs(start_coord.last - end_coord.last) != ship_length - 1
+      def check_vertical_ship_length(ship_length)
+        if (start_coord[1].ord - end_coord[1].ord).abs != ship_length - 1
           "ship is invalid: incorrect ship length"
         end
       end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -20,26 +20,55 @@ class ShipTest < Minitest::Test
     assert_equal "B1", ship.end_coord
   end
 
-  def test_ship_orientation
-    skip
-    vertical_ship = "A1 B1"
-    horizontal_ship = "A1 A2"
+  def test_check_horizontal_ship_length
+    horizontal_ship = Ship.new
+    horizontal_ship.coordinates("A1 A2")
+    error_output = "ship is invalid: incorrect ship length"
+    invalid_input = horizontal_ship.check_horizontal_ship_length(3)
+
+    assert_equal error_output, invalid_input
   end
 
   def test_horizontal_ship
+    horizontal_ship = Ship.new
+    horizontal_ship.coordinates("A1 A2")
+    error_output = "ship is invalid: incorrect ship length"
+    invalid_input = horizontal_ship.horizontal_ship(3)
 
-  end
-
-  def test_check_horizontal_ship_length
-
-  end
-
-  def test_vertical_ship
-
+    assert_equal error_output, invalid_input
   end
 
   def test_check_vertical_ship_length
+    vertical_ship = Ship.new
+    vertical_ship.coordinates("A1 B1")
+    error_output = "ship is invalid: incorrect ship length"
+    invalid_input = vertical_ship.check_vertical_ship_length(3)
 
+    assert_equal error_output, invalid_input
+  end
+
+  def test_vertical_ship
+    horizontal_ship = Ship.new
+    horizontal_ship.coordinates("A1 A2")
+    error_output = "ship is invalid: incorrect ship length"
+    invalid_input = horizontal_ship.vertical_ship(3)
+
+    assert_equal error_output, invalid_input
+  end
+
+  def test_ship_orientation
+    vertical_ship = Ship.new
+    vertical_ship.coordinates("A1 B1")
+    horizontal_ship = Ship.new
+    horizontal_ship.coordinates("A1 A2")
+    diagonal_ship = Ship.new
+    diagonal_ship.coordinates("A1 B2")
+    error_output = "ship is invalid: incorrect ship length"
+    diagonal_error = "ship is invalid: cannot place diagonal"
+
+    assert_equal error_output, vertical_ship.ship_orientation(3)
+    assert_equal error_output, horizontal_ship.ship_orientation(3)
+    assert_equal diagonal_error, diagonal_ship.ship_orientation(3)
   end
 
 end
