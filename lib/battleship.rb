78b8b22ffec74589b677dd6727c867dd6de60430
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-06 19:56:30 -0600

    add battleship and test, add referee class

---
+++ lib/battleship.rb
@@ -0,0 +1,4 @@
+puts 'Welcome to BATTLESHIP'
+
+puts 'Would you like to (p)lay, read the (i)nstructions, or (q)uit?'
+user_input = gets
--- test/referee_test.rb
+++ test/referee_test.rb
@@ -1,2 +1,12 @@
 require './test/test_helper'
 require_relative '../lib/referee'
+
+class RefereeTest < Minitest::Test
+
+  def test_referee_exists
+    referee = Referee.new
+
+    assert_instance_of Referee, referee
+  end
+
+end
